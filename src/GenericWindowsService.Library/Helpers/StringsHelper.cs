d3df00e177afcc26824bd10f3a8c7d15a233e68f
Habilya <ciuvilin.co@gmail.com>
2024-07-01 11:38:48 -0400

    Add StringsHelper and Tests

---
+++ src/GenericWindowsService.Library/Helpers/StringsHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace GenericWindowsService.Library.Helpers;
+
+public static class StringsHelper
+{
+	public static string RemoveDiacritics(string input)
+	{
+		var normalizedString = input.Normalize(NormalizationForm.FormD);
+		var stringBuilder = new StringBuilder(capacity: normalizedString.Length);
+
+		foreach (var c in normalizedString)
+		{
+			var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+			if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+			{
+				stringBuilder.Append(c);
+			}
+		}
+
+		return stringBuilder
+			.ToString()
+			.Normalize(NormalizationForm.FormC);
+	}
+}
---
+++ test/GenericWindowsService.Library.Tests.Unit/StringsHelperTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using GenericWindowsService.Library.Helpers;
+
+namespace GenericWindowsService.Library.Tests.Unit;
+
+public class StringsHelperTests
+{
+	[Theory]
+	[InlineData("été", "ete")]
+	[InlineData("Aujourd'hui", "Aujourd'hui")]
+	[InlineData("The Meaning of Ça", "The Meaning of Ca")]
+	[InlineData("ÉTÉ", "ETE")]
+	[InlineData("Allô", "Allo")]
+	[InlineData("Noël", "Noel")]
+	[InlineData("NOËL", "NOEL")]
+	[InlineData("ça", "ca")]
+	[InlineData("Ça", "Ca")]
+	[InlineData("è", "e")]
+	[InlineData("être", "etre")]
+	[InlineData("Maïs", "Mais")]
+	public void RemoveDiacritics_ShouldRemoveDiacritics_WhenDiacriticsPresentInString(string input, string expected)
+	{
+		// Arrange
+
+		// Act
+		var actual = StringsHelper.RemoveDiacritics(input);
+
+		// Assert
+		actual.Should().Be(expected);
+	}
+}
